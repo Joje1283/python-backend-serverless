4c26d0f5f53875ffe70eddda0a1f4aef0bae24ac
Joje1283 <joje1283@gmail.com>
2025-05-29 22:59:17 +0900

    serverless 배포 설정 추가

---
+++ restapi/tests/test_root.py
@@ -0,0 +1,14 @@
+import pytest
+from fastapi.testclient import TestClient
+from app import app
+
+@pytest.fixture
+def client():
+    return TestClient(app)
+
+
+@pytest.mark.asyncio
+async def test_root(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello from Serverless FastAPI!"}
