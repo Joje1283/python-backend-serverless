046ce7dd364a78914bd75c145e27747c42642756
Joje1283 <joje1283@gmail.com>
2025-05-31 11:48:03 +0900

    테스트 변경

--- restapi/tests/test_root.py
+++ restapi/tests/test_root.py
@@ -7,8 +7,7 @@ def client():
     return TestClient(app)
 
 
-@pytest.mark.asyncio
-async def test_root(client):
+def test_root(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello from Serverless FastAPI!"}
