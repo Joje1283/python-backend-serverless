186667f3677d829bcb8fd93ce9a9849331bedf60
Jaesik <joje1283@gmail.com>
2025-05-31 11:49:07 +0900

    Add serverless based restapi (#1)
    * 패키지 설치, fastapi app, mangum handler 추가
    
    * serverless 배포 설정 추가
    
    * 테스트 파라미터 변경
    
    * npm 의존성 추가
    
    * requirements.txt의 빈줄 제거
    
    * requirements.txt를 utf-8로 변경
    
    * 배포 조건 브랜치 변경
    
    * 테스트 변경

---
+++ restapi/tests/test_root.py
@@ -0,0 +1,13 @@
+import pytest
+from fastapi.testclient import TestClient
+from app import app
+
+@pytest.fixture
+def client():
+    return TestClient(app)
+
+
+def test_root(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello from Serverless FastAPI!"}
