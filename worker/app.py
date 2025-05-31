1d97fcd75989e71962b17b9311f2faed5ad1661e
Joje1283 <joje1283@gmail.com>
2025-05-31 12:57:49 +0900

    워커 함수 추가

--- worker/app.py
+++ worker/app.py
@@ -1,29 +1,18 @@
+from enum import StrEnum
 from chalice import Chalice
 
 app = Chalice(app_name='worker')
 
-
-@app.route('/')
-def index():
-    return {'hello': 'world'}
+class QueueName(StrEnum):
+    push = "push"
 
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+@app.on_sqs_message(QueueName.push, batch_size=1)
+def push_handler(event):
+    for record in event:
+        body = record.body
+        # Assuming body is a JSON string, you can parse it here if needed
+        print(f"Received message: {body}")
+        # Process the message as needed
+        # For example, you could log it or perform some action based on its content
+
